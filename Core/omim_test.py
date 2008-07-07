021191650d8a900bba62a68486d9e1953e13dc15
Xiaodi Wu <xiaodi.wu@gmail.com>
2008-07-07 17:59:19 -0400

    Added OMIM parsing scripts to repository.

---
+++ Core/omim_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# Filename: omim_text.py
+
+# INPUT: OMIM (txt)
+# for each entry in txt:
+#	count allelic variants
+# ---
+# This code is part of the Trait-o-matic project and is governed by its license.
+
+import sys
+import omim
+
+def main():
+	f = omim.input(sys.argv[1])
+	av_count = 0
+	for record in f:
+		if record.allelic_variants is not None:
+			av_count += len(record.allelic_variants)
+	print av_count
+
+if __name__ == "__main__":
+	main()
